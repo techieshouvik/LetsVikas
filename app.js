3b9c1436ae5bfe23e347d105171304aacf8595d7
gaurangsh <60140780+gaurangsh@users.noreply.github.com>
2020-10-24 16:11:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const navSlide=()=> {
     
     const burger=document.querySelector('.burger');
     const nav=document.querySelector('.nav-items');
-    
+    // I have put a comment
     burger.addEventListener('click',()=>{
         
         nav.classList.toggle('burger-active');
